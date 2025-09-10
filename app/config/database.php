72c71997bd1784edd02abe0e84a05d796a4370a5
andrea pagal <pagaldrea@gmail.com>
2025-09-10 16:47:07 +0800

    com

--- app/config/database.php
+++ app/config/database.php
@@ -63,7 +63,7 @@ $database['main'] = array(
     'port'		=> '3306',
     'username'	=> 'sql12798027',
     'password'	=> 'DFCP4lXGJW',
-    'database'	=> 'students',
+    'database'	=> 'sql12798027 ',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
